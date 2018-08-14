15dccb5cf38d3f69be001e9554431f9e67e8c38d
Mohammed Ghouse <shaik.mohammed@youngwonks.com>
2018-08-14 08:25:06 +0000

    edit , update , show , index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,16 @@
 class ArticlesController < ApplicationController
   
+  def index 
+    @grab_articles = Article.all
+  end
+  
+  
   def new 
     @article = Article.new
+  end
+  
+  def edit 
+    @article = Article.find(params[:id])
   end
   
   
@@ -18,6 +27,17 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article successfully updted"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+      
   end
   
   private
