9dc604b0ad6d58ded62a035d9c82570446b07e9e
Mohammed Ghouse <shaik.mohammed@youngwonks.com>
2018-08-16 14:16:00 +0000

    user validations

--- app/models/use.rb
+++ app/models/use.rb
@@ -1,3 +1,5 @@
 class Use < ActiveRecord::Base
-  
+  validates :username, presence:true, uniqueness: {case_sensitive: false}, length:{minimum: 3, maximum: 25}
+  validates :email, presence:true, uniqueness: {case_sensitive: false}, length:{maximum: 25}
+   
 end
