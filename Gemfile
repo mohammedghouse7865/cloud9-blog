70202865bc4b3f9abf7834e2735587305960c801
Mohammed Ghouse <shaik.mohammed@youngwonks.com>
2018-08-17 01:41:00 +0000

    setup rspec capybara

--- Gemfile
+++ Gemfile
@@ -33,11 +33,18 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+  gem 'rspec-rails','3.1.0'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 end
 
+group :test do
+  gem 'capybara','2.7.1'
+  
+end
+
 group :development do
+
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180816134433) do
+ActiveRecord::Schema.define(version: 20180816141825) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "usage_people", force: :cascade do |t|
